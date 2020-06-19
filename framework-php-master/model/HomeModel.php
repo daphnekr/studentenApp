68f6eb138aef4cdd7ca068bb0dcf3785113371ff
JeremyK <99058701@mydacinci.nl>
2020-06-19 23:47:45 +0200

    createStudent ge-update naar werkend

--- framework-php-master/model/HomeModel.php
+++ framework-php-master/model/HomeModel.php
@@ -15,7 +15,7 @@ function newStudent($data1, $data2, $data3)
 {
 	$conn = openDatabaseConnection();
 	
-	$stmt = $conn->prepare("INSERT INTO studenten (`voornaam`, `achternaam`, `e-mail`, `slb'er_id`, `klas_id`) VALUES (:VN, :AN, :EM, '1', '1')");
+	$stmt = $conn->prepare("INSERT INTO studenten (voornaam, achternaam, `e-mail`, `slb'er_id`, klas_id) VALUES (:VN, :AN, :EM, 1, 1)");
 	$stmt->bindParam(":VN", $data1);
 	$stmt->bindParam(":AN", $data2);
 	$stmt->bindParam(":EM", $data3);
@@ -23,7 +23,6 @@ function newStudent($data1, $data2, $data3)
 	
 	$conn = null;
 	
-	return $stmt->fetchAll();
 }
 
 
