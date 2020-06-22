901312d31a8d8e09c2a2f550bd025e63d8a956c8
Daphne Kramer <99046027@mydavinci.nl>
2020-06-22 20:23:41 +0200

    footer toegevoegd en index overzicht van studenten per klas

--- framework-php-master/view/home/index.php
+++ framework-php-master/view/home/index.php
@@ -1,18 +1,28 @@
 <div class="container">
  <h1 class = "text-danger"> Overzicht studenten per klas</h1>
 
+<div class = "row">
+<?php foreach ($groups as $group){ ?>
+    <div class = "col">
+        <h2><?= $group['groepnaam']; $id = $group['id']?> </h2>
+        <hr>
+        <?php 
+        foreach($students as $student){
+            if ($id == $student['klas_id']){
+                ?>
+                Voornaam: <?= $student['voornaam']; ?> <br>
+                Achternaam: <?= $student['achternaam']; ?> <br>
+                E-mailadres: <?= $student['e-mail']; ?> <br>
+                Klas: <?= $student['groepnaam']; ?> 
+                <hr>
+            
+            <?php
+            }
+        }
+        ?>
+    </div>
 <?php
- foreach($students as $student){
+  }
 ?>
-    Voornaam: <?= $student['voornaam']; ?> <br>
-    Achternaam: <?= $student['achternaam']; ?> <br>
-    E-mailadres: <?= $student['email']; ?> <br>
-    Klas: <?= $student['groepnaam']; ?> 
-    <hr>
-
-<?php
-
- }
-?>
-
+</div>
 </div>
--- framework-php-master/view/templates/footer.php
+++ framework-php-master/view/templates/footer.php
@@ -1,3 +1,9 @@
+<footer class="page-footer bg-secondary text-white border">
 
+  <div class="text-center">
+    © 2020 Daphne Kramer & Jeremy Korteland
+  </div>
+
+</footer>
 </body>
 </html>
