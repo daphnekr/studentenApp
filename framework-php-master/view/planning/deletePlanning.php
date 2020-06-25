cdee70fe96eb17ec8667699d027b32bfd4e43e0f
Daphne Kramer <99046027@mydavinci.nl>
2020-06-25 17:14:50 +0200

    delete planning aangemaakt en deleteStudent error opgelost

---
+++ framework-php-master/view/planning/deletePlanning.php
@@ -0,0 +1,9 @@
+<div class = "container">
+    <h1> Planning verwijderen </h1>
+    <p><span>Weet je zeker dat je <?= $planning['voornaam'], ' ', $planning['achternaam']; ?> bij <?= $planning['les']; ?> om <?= date('H:i', strtotime($planning["tijd"])); ?> uur wilt verwijderen?</span></p>
+
+    <div> 
+        <a href= "<?= URL ?>planning/agenda" class  = "btn btn-info">Nee</a>
+        <a href= "<?= URL ?>planning/destroyPlanning/<?= $planning['planning_id']; ?>" class = "btn btn-danger">Ja</a>
+    </div>
+</div>
